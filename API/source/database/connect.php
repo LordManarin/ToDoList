1f5f579079d52e598d043bd2cbf9e3c5f9ed9a16
RafaelManarin <rafaelmanarin@outlook.com>
2020-05-13 16:15:06 -0300

    adicionado SLIM framework

--- API/source/database/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "config.php";
-require "createDB.php";
-require "createTable.php";
-
-criarDB($servername, $username, $password,$dbname);
-echo "</br>";
-criaTabela($servername, $username, $password, $dbname);
