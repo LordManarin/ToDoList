68210686814eb0262ed2bb61837977c4df2af26c
RafaelManarin <rafaelmanarin@outlook.com>
2020-05-13 16:47:22 -0300

    separacao de arquivos

---
+++ API/public/Tarefas/ExibeTarefas.php
@@ -0,0 +1,35 @@
+<?php
+use Slim\Factory\AppFactory;
+use Source\Models\Tarefa;
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . "/../config.php";
+require __DIR__ . "/../Models/Tarefa.php";
+require __DIR__ . "/../Models/Validations.php";
+
+public function ExibeTarefa ($request, $response)
+{
+
+    $app = AppFactory::create();
+
+    $app->addRoutingMiddleware();
+    $app->addErrorMiddleware(true, true, true);
+
+
+    $usuarioId = filter_input(INPUT_GET, "usuario_id");
+    header("HTTP/1.1 200 ok");
+    if (!$usuarioId) {
+        header("HTTP/1.1 201 Sucess");
+        echo json_encode(array("response" => "Nenhuma tarefa localizada"));
+        exit;
+    } else {
+        $usuario = new Tarefa();
+        $return = array();
+        foreach ($usuario->find("usuario_id =:usuario_id", "usuario_id=$usuarioId")->fetch(true) as $usuario) {
+
+            array_push($return, $usuario->data());
+        }
+        echo json_encode(array("response" => $return));
+    }
+    return $response;
+
+}
---
+++ API/public/Tarefas/PostaTarefas.php
@@ -0,0 +1,52 @@
+<?php
+use Source\Models\Tarefa;
+use Source\Models\Validations;
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . "/../config.php";
+require __DIR__ . "/../Models/Tarefa.php";
+require __DIR__ . "/../Models/Validations.php";
+
+public function PostaTarefa($request, $response){
+    $data= json_decode(file_get_contents("php://input"));
+    // se nao forem enviados dados, vai apresentar esta mensagem de erro
+    if(!$data){
+        header("HTTP/1.1 400 BAD REQUEST");
+        echo json_encode(array("response"=>"Nenhum dado informado"));
+        exit;
+    }
+    $errors=array();
+    // testa se todos os campos estao preenchidos
+    if(!Validations::validationInteger($data->usuario_id)) {
+        array_push($errors, "usuario_id invalido");
+    }
+    if(!Validations::validationsString($data->tarefa)){
+        array_push($errors, "Tarefa invalido");
+    }
+    if(!Validations::validationsString($data->descricao)){
+        array_push($errors, "Descrição invalido");
+    }
+    if(!Validations::validationsString($data->concluido)){
+        array_push($errors, "Estado Invalido");
+    }
+    if(count($errors)>0){
+        header("HTTP/1.1 400  BAD REQUEST");
+        echo json_encode(array("responde"=>"Campos invalidos!", "fields"=>$errors));
+        exit;
+    }
+    // se tudo OK, envia os dados para o DB
+    $tarefa = new Tarefa();
+    $tarefa->usuario_id = $data->usuario_id;
+    $tarefa->tarefa = $data->tarefa;
+    $tarefa->descricao = $data->descricao;
+    $tarefa->concluido = $data->concluido;
+    $tarefa->save();
+    if ( $tarefa->fail()) {
+        header("HTTP/1.1 500 Internal Server Error");
+        echo json_encode(array("response" =>  $tarefa->fail()->getMessage()));
+        exit;
+    }
+    // se der tudo certo, informa esta mensagem
+    header("HTTP/1.1 200 CREATED");
+    echo json_encode(array("response" => "Tarefa Criada com Sucesso"));
+    return $response;
+};
