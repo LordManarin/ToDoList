f42628a7c02af52fd95f9d2fa902e338f9391f46
RafaelManarin <rafaelmanarin@outlook.com>
2020-05-15 16:57:43 -0300

    Adicionado comentarios

--- API/public/index.php
+++ API/public/index.php
@@ -10,21 +10,23 @@ $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
+
+/* EXIBE AS TAREFAS FILTRANDO COM BASE NO usuario_id */
 $app->get('/', function (Request $request, Response $response): Response {
     ExibeTarefa($response);
     return $response;
 });
-
+/* INSERE UMA TAREFA NOVA */
 $app->post("/", function(Request $request, Response $response, $args): Response {
     PostaTarefa($response);
     return $response;
 });
-
+/* ATUALIZA UMA TAREFA EXISTENTE*/
 $app->put("/", function(Request $request, Response $response, $args): Response {
     AtualizaTarefas($response);
     return $response;
 });
-
+/* DELETA UMA TAREFA EXISTENTE*/
 $app->delete("/", function(Request $request, Response $response, $args): Response {
     DeletaTarefas($response);
     return $response;
