08eff761777183fa5438da3010e0cb903275a187
RafaelManarin <rafaelmanarin@outlook.com>
2020-05-22 08:59:14 -0300

    autenticacao basica com tratamento de erros

--- API/src/AuthBasico.php
+++ API/src/AuthBasico.php
@@ -6,6 +6,14 @@ function AuthBasico(): HttpBasicAuthentication{
     return new httpBasicAuthentication([
         "users"=> [
             "root"=> "teste"
-            ]
+            ],
+        "error" => function ($response, $arguments) {
+            $data = [];
+            $data["response"] = "Usuario e senha incorretos";
+            $body = $response->getBody();
+            $body->write(json_encode($data, JSON_UNESCAPED_SLASHES));
+            return $response->withBody($body);
+        }
         ]);
+
 }
