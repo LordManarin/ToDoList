bf313e225d3ab21aa14dbc70d5757e9c074f789b
RAFAEL MANARIN <rafaelmanarin@outlook.com>
2020-06-01 12:20:09 +0000

    ValidateInfos.php edited online with Bitbucket

--- API/src/Domain/Controllers/ValidateInfos.php
+++ API/src/Domain/Controllers/ValidateInfos.php
@@ -20,10 +20,21 @@ class ValidateInfos{
             array_push($errors, "Estado Invalido");
         }
         if(count($errors)>0){
-            echo json_encode(array("responde"=>"Campos invalidos!", "fields"=>$errors));
+            print json_encode(array("responde"=>"Campos invalidos!", "fields"=>$errors));
             exit;
         }else{
             return $data;
         }
     }
+
+    public static function validateTaskId($taskId){
+        if(!$taskId) {
+            echo json_encode(array("response" => "Nenhuma tarefa localizada"));
+            exit;
+        }else{
+            return $taskId;
+        }
+    }
+
+
 }
